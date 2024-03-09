3bf0c627df462620eb3b9d7b8b68798676eb5450
rmmcosta <ricardocosta101085@gmail.com>
2024-03-09 10:27:05 +0000

    spring initializr

---
+++ src/main/kotlin/com/rmmcosta/bookreadskotlin/BookReadsKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.rmmcosta.bookreadskotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookReadsKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookReadsKotlinApplication>(*args)
+}
